44630eb5a5e06f34615e3c14c441efac2c7e955d
hta393939 <hta393939@gmail.com>
2023-09-18 16:59:44 +0900

    WIP: commit

--- public/motion/index.js
+++ public/motion/index.js
@@ -3,15 +3,25 @@
  */
 
 class Misc {
-    init() {
+    async init() {
         console.log('init');
         
         const canvas = document.getElementById('maincanvas');
         if (canvas) {
-
+            canvas.width = 640;
+            canvas.height = 360;
+            const param = {
+                canvas,
+            };
+            const renderer = new Renderer();
+            await renderer.init(param);
+            requestAnimationFrame(() => {
+                renderer.update();
+            });
         } else {
             
         }
+
     }
 }
 
--- public/motion/renderer.js
+++ public/motion/renderer.js
@@ -1,11 +1,66 @@
+/**
+ * @file renderer.js
+ */
+// need threejs
 
 (function(_global) {
 
 class Renderer {
+    constructor() {
+
+    }
+
+    async init(param) {
+        const renderer = new THREE.WebGLRenderer({
+            preserveDrawingBuffer: true,
+            canvas: param.canvas,
+        });
+        this.renderer = renderer;
+
+        const scene = new THREE.Scene();
+        this.scene = scene;
+
+        {
+            const camera = new THREE.PerspectiveCamera(45, 16/9, 1, 1000);
+            this.camera = camera;
+            camera.position.set(1, 2, 5);
+            camera.lookAt(new THREE.Vector3(0, 1, 0));
+        }
+
+        /*
+        { // object, dom
+            const control = new THREE.OrbitControls(this.camera, renderer.domElement);
+            this.control = control;
+        }
+        */
+        { // object, dom
+            const control = new THREE.TrackballControls(this.camera, param.canvas);
+            this.control = control;
+        }
+
+        {
+            const grid = new THREE.GridHelper(10, 5);
+            grid.name = 'grid';
+            scene.add(grid);
+        }
+    }
+
+/**
+ * 外部から呼ぶ
+ */
+    update() {
+        if (this.control) {
+            this.control.update();
+        }
+
+        if (this.camera) {
+            this.renderer.render(this.scene, this.camera);
+        }
+    }
 
 }
 
-globalThis.Renderer = Renderer;
+_global.Renderer = Renderer;
 
 })(globalThis);
 
