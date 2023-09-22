a133430b4ec9d06dd37c5b4d6de940cc5dee2465
hta393939 <hta393939@gmail.com>
2023-09-22 20:31:16 +0900

    WIP: mesh parse

--- public/lib/gpbxmlparser.js
+++ public/lib/gpbxmlparser.js
@@ -4,9 +4,30 @@
 
 (function(_global) {
 
+/**
+ * 文字列をスペースで分解して，数値になった分だけ配列として返す．
+ * 1つしか無い場合は数値を返す．
+ * @param {string} s 
+ * @returns {number[] | number}
+ */
+const toFloats = (s) => {
+    const ss = s.trim().split(' ');
+    const ret = [];
+    for (const val of ss) {
+        const f = Number.parseFloat(val);
+        if (Number.isFinite(f)) {
+            ret.push(f);
+        }
+    }
+    if (ret.length === 1) {
+        return ret[0];
+    }
+
+    return ret;
+};
+
 class GPBXMLParser {
     constructor() {
-
     }
 
 /**
@@ -23,7 +44,8 @@ class GPBXMLParser {
             console.log('reference', refnode);
             for (const node of refnode.children) {
                 const name = node.nodeName.toLowerCase();
-                ref[name] = node.textContent;
+                const val = node.textContent;
+                ref[name] = (name === 'xref') ? val : Number.parseInt(val);
             }
             ret.references.push(ref);
         }
@@ -35,8 +57,60 @@ class GPBXMLParser {
  * @param {HTMLElement}
  * @returns 
  */
-    parseMesh(node) {
-        const ret = {};
+    parseMesh(innode) {
+        const ret = {
+            vertexelements: [],
+            vertices: [],
+        };
+        ret.id = innode.id;
+        for (const node of innode.children) {
+            const name = node.nodeName.toLowerCase();
+            switch(name) {
+            case 'vertexelement':
+                {
+                    const ve = {};
+                    for (const node2 of node.children) {
+                        const name2 = node2.nodeName.toLowerCase();
+                        const val2 = node2.textContent;
+                        ve[name2] = (name2 === 'usage') ? val2 : Number.parseInt(val2);
+                    }
+                    ret.vertexelements.push(ve);
+                }
+                break;
+            case 'vertices':
+                {
+                    const val = node.textContent;
+                    const lines = val.split('\n');
+                    console.log('lines num', lines.length);
+                    for (const line of lines) {
+                        if (line == '') {
+                            continue;
+                        }
+                        if (line.startsWith('//')) {
+                            continue;
+                        }
+                        const fs = toFloats(line);
+                        console.log(fs);
+                        break;
+                    }
+                }
+                break;
+            case 'bounds':
+                {
+                    const bound = {};
+                    for (const node2 of node.children) {
+                        const name2 = node2.nodeName.toLowerCase();
+                        const val2 = node2.textContent;
+                        bound[name2] = toFloats(val2);
+                    }
+                    ret.bounds = bound;
+                }
+                break;
+            case 'meshpart':
+                break;
+            }
+        }
+
         return ret;
     }
 
@@ -45,7 +119,8 @@ class GPBXMLParser {
  * @param {HTMLElement} node 
  */
     parseNode(node) {
-
+        const ret = {};
+        return ret;
     }
 
 /**
@@ -60,11 +135,12 @@ class GPBXMLParser {
 
 /**
  * 
- * @param {HTMLElement} node 
+ * @param {HTMLElement} innode 
  * @returns 
  */
-    parseAnimations(node) {
+    parseAnimations(innode) {
         const ret = {};
+        ret.id = innode.id;
         return ret;
     }
 
@@ -104,7 +180,8 @@ class GPBXMLParser {
             case 'mesh':
                 {
                     const result = this.parseMesh(node);
-                    meshes.push(node)
+                    meshes.push(result);
+                    console.log('mesh', result);
                 };
                 break;
             case 'scene':
